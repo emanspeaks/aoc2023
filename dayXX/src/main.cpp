e00af437cdc75038782244a5ed1eb707ad0aabc2
Randy Eckman <randy.eckman@nasa.gov>
2023-12-03 04:12:47 -0600

    adding boilerplate templates to use for each day

---
+++ dayXX/src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+#include "main.h"
+#include "aoc.h"
+
+int main(int argc, char const *argv[]) {
+  std::ifstream ifs("input.txt");
+  std::string line;
+
+  int sum = 0, sumb = 0;
+  if (ifs.is_open()){
+    while (ifs) {
+      std::getline(ifs, line);
+      if (line.length()) {
+
+      }
+    }
+  } else {
+    std::cout << "failed to open input.txt";
+    return 1;
+  }
+
+  std::cout << AOC_DAY << " A\n";
+  std::cout << sum << "\n";
+
+  std::cout << AOC_DAY << " B\n";
+  std::cout << sumb << "\n";
+
+  return 0;
+}
