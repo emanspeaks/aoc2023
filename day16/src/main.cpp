d7a5645dd93eb1e9ffe5f8b55bc88c79be846cab
Randy Eckman <randy.eckman@nasa.gov>
2023-12-15 00:14:23 -0600

    prep for day 16

---
+++ day16/src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+#include "main.h"
+#include "aoc.h"
+#include "thing.h"
+
+int main(int argc, char const *argv[]) {
+  ConsoleUTF8 set_console_to_utf8;
+  std::ifstream ifs("input.txt");
+  std::string line;
+  Thing th;
+
+  int sum = 0, sumb = 0;
+  if (ifs.is_open()){
+    while (ifs) {
+      std::getline(ifs, line);
+      if (line.length()) {
+        th.parseLine(line);
+      }
+    }
+  } else {
+    std::cout << "failed to open input.txt";
+    return 1;
+  }
+  ifs.close();
+
+  std::cout << AOC_DAY << " A\n";
+  std::cout << sum << "\n";
+
+  std::cout << AOC_DAY << " B\n";
+  std::cout << sumb << "\n";
+
+  return 0;
+}
